dbceec1e72364f1125756e8409d79a6a1111b8fa
VictorBui2702 <victorbui1990@gmai.com>
2019-03-28 15:23:25 +0700

    Add initial feature

--- src/App.js
+++ src/App.js
@@ -2,7 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const clientId = process.env.REACT_APP_CLIENT_ID;
+
 class App extends Component {
+  constructor() {
+    super();
+    const existingToken = sessionStorage.getItem('token');
+    const accessToken = (window.location.search.split("=")[0] === "?access_token") ? window.location.search.split("=")[1] : null;
+  
+    if (!accessToken && !existingToken) {
+      window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`)
+    }
+  
+    if (accessToken) {
+      console.log(`New accessToken: ${accessToken}`);
+  
+      sessionStorage.setItem("token", accessToken);
+      this.state = {
+          token: accessToken
+      }
+    }
+  
+    if (existingToken) {
+      this.state = {
+        token: existingToken
+      };
+    }    
+  }
+  
+  
   render() {
     return (
       <div className="App">

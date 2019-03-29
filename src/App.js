8835f645cb4887a44256892030d3b9a0cba97892
Nat <natalie.hoang91@gmail.com>
2019-03-29 16:01:31 +0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import SearchBox from './components/tool/SearchBox'
-import Pagination from './components/pagination/Pagination'
-import SearchResults from './components/page/SearchResults'
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import SearchBox from "./components/tool/SearchBox";
+import Pagination from "./components/pagination/Pagination";
+// import SearchResults from "./components/page/SearchResults";
+import IssueDetail from "./components/page/IssueDetail";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
 const apiURL = `https://api.github.com`;
@@ -11,11 +13,16 @@ const apiURL = `https://api.github.com`;
 class App extends Component {
   constructor(props) {
     super(props);
-    const existingToken = sessionStorage.getItem('token');
-    const accessToken = (window.location.search.split("=")[0] === "?access_token") ? window.location.search.split("=")[1] : null;
+    const existingToken = sessionStorage.getItem("token");
+    const accessToken =
+      window.location.search.split("=")[0] === "?access_token"
+        ? window.location.search.split("=")[1]
+        : null;
 
     if (!accessToken && !existingToken) {
-      window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`)
+      window.location.replace(
+        `https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`
+      );
     }
 
     if (accessToken) {
@@ -25,10 +32,10 @@ class App extends Component {
       this.state = {
         token: accessToken,
         issues: [],
-        owner: '',
-        repo: '',
-        search: '',
-        page: ''
+        owner: "",
+        repo: "",
+        search: "",
+        page: ""
       };
     }
 
@@ -36,38 +43,35 @@ class App extends Component {
       this.state = {
         token: existingToken,
         issues: [],
-        owner: 'facebook',
-        repo: 'react',
-        search: '',
+        owner: "facebook",
+        repo: "react",
+        search: "",
         page: 1
       };
     }
   }
 
   githubAPI = async () => {
-    let { owner, repo, token, page } = this.state
-    let response = await fetch(`${apiURL}/repos/${owner}/${repo}/issues?access_token=${token}&page=${page}&per_page=20`);
+    let { owner, repo, token, page } = this.state;
+    let response = await fetch(
+      `${apiURL}/repos/${owner}/${repo}/issues?access_token=${token}&page=${page}&per_page=20`
+    );
     let issues = await response.json();
-    this.setState({
-      issues: issues
-    }, () => console.log(this.state.issues))
-  }
+    this.setState(
+      {
+        issues: issues
+      },
+      () => console.log(this.state.issues)
+    );
+  };
 
   componentDidMount() {
     this.githubAPI();
   }
- 
+
   render() {
-    if (this.state.issues.length > 0)
-      return (
-        <div>
-          <SearchBox />
-          <Pagination />
-          <SearchResults issues={this.state.issues} />
-        </div>
-      );
-      else 
-      return (<h2>Loading...!</h2>)
+    if (this.state.issues.length > 0) return <div />;
+    else return <h2>Loading...!</h2>;
   }
 }
 

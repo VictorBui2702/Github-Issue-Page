cccc045dd3b51488b21ee23f006bd10d33e98f0d
Phong Vu <cafeine@JPs-mac.local>
2019-03-30 16:01:28 +0700

    tried to work on the search Repo but it didnt work. Help needed guys

--- src/components/page/SearchResults.js
+++ src/components/page/SearchResults.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap';
 import Profile from '../profile/Profile'
-import Labels from '../issues/Labels'
 import IssueOverview from '../issues/IssueOverview'
 
 const ColorLine = ({ color }) => (
-    <hr style={{color: color, backgroundColor: color,height: 3}}/>
+    <hr style={{color: color, backgroundColor: color,height: 0.2}}/>
 );
 
 export default class SearchResults extends Component {
@@ -20,7 +19,7 @@ export default class SearchResults extends Component {
                         </Col>
                         <Col sm={4} className="issues-page">
                             <Profile profile={issue.user} />
-                            <Labels labels={issue.labels} />
+                            
                         </Col>
                         <Col ><ColorLine color="lightpink"/></Col>
                     </Row>

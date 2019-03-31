5ac38f7712f6fd29ab3e6a211ed8190303e3d593
Nat <natalie.hoang91@gmail.com>
2019-03-31 23:52:53 +0700

    update UI

--- src/components/tool/SearchBox.js
+++ src/components/tool/SearchBox.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class SearchBox extends Component {
-    render() {
-        return (
-            <span className="d-flex align-items-center px-3 py-1">
-            <form name="searchBox ">
-                <input placeholder="search for owner/repo" value={this.props.search} onChange={this.props.searchIssues} />
-                <input type="submit" value="search" onClick={this.props.submitSearchIssues} />
-            </form>
-            </span>
-        )
-    }
+  render() {
+    return (
+      <div className="row justify-content-center my-1">
+        <form name="searchBox">
+          <input
+            className="searchbox-style"
+            placeholder="search for owner/repo"
+            value={this.props.search}
+            onChange={this.props.searchIssues}
+          />
+          <input
+            className="btn-submit-style"
+            type="submit"
+            value="search"
+            onClick={this.props.submitSearchIssues}
+          />
+        </form>
+      </div>
+    );
+  }
 }
